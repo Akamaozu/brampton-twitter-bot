b19653266e88d9152a7a4b3aec19dca307a7e25a
Uzo Olisemeka <uzo@designbymobius.ca>
2020-05-26 09:24:21 -0400

    base-station: log port http server connects to
    minor code cleanup

--- base-station/components/http-server.js
+++ base-station/components/http-server.js
@@ -7,29 +7,31 @@ var citizen = require('supe'),
 
     connection_status = false;
 
-server.listen( process.env.PORT || 5000 );
+var target_port = process.env.PORT || 5000;
+
+server.listen( target_port, function( error ){
+  if( error ) throw error;
+  else console.log( 'action=listen-to-port success=true port=' + target_port );
+});
 
 express.use( require('express').static( path.join( __dirname, '..', 'webapp' ) ) );
 
 express.get('/', function(){
-
   var markup_cache;
 
   return function( req, res ){
-
-    if( !markup_cache ) markup_cache = fs.readFileSync( path.join( __dirname, '..', 'webapp', 'index.html' ), 'utf8' );
+    if( ! markup_cache ) markup_cache = fs.readFileSync( path.join( __dirname, '..', 'webapp', 'index.html' ), 'utf8' );
     res.send( markup_cache );
   };
 
 }());
 
 socketio.on( 'connection', function( socket ){
-
   var name;
 
   console.log( 'new socket.io client connection' );
 
-  socket.on( 'connection-status-request', function(){    
+  socket.on( 'connection-status-request', function(){
     socket.emit( 'connection-status', connection_status );
   });
 
@@ -38,7 +40,6 @@ socketio.on( 'connection', function( socket ){
   });
 
   socket.on( 'file-upload', function( data ){
-
     console.log( 'action=upload satellite=' + name + ' file=' + data.name );
     socketio.emit( 'new-upload', { uploader: name, content: new Buffer( data.content ).toString('base64') });
   });
@@ -46,6 +47,6 @@ socketio.on( 'connection', function( socket ){
 
 citizen.noticeboard.watch( 'toggle-connection-status', 'do-toggle', function(){
 
-  connection_status = !connection_status;
+  connection_status = ! connection_status;
   socketio.emit( 'connection-status', connection_status );
 });
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var supe = require('supe')(),
     path = require('path'),
     path_to_root = '.';
 
+// load .env file
+  require('dotenv').config();
+
 require( path_to_root + '/utils/prefix-stdout-with-timestamp' )();
 
 supe.start( 'base-station', path_to_root + '/base-station/base-station' );
