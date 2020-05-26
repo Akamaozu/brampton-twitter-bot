8858f42eb92cea89e99287b618a8f737efad698d
Uzo Olisemeka <uzo@designbymobius.ca>
2020-05-26 07:43:24 -0400

    modify satellite and base-station to share package.json deps

--- weather-satellite/satellite.js
+++ weather-satellite/satellite.js
@@ -4,15 +4,10 @@ var path = require('path'),
     supervisor = require('supe')({
       retries: process.env.CITIZEN_REVIVES_PER_DURATION,
       duration: process.env.CITIZEN_REVIVE_DURATION_MINS
-    }),
-
-    log_citizen_output_addon = require( 'supe-addon-log-citizen-output' ),
-    log_citizen_lifecycle_addon = require( 'supe-addon-log-citizen-lifecycle' );
+    });
 
 // load helpers to make supervising components easier
   supervisor.use( require( './utils/supervisor/prefix-output-with-timestamp' ) );
-  supervisor.use( log_citizen_lifecycle_addon );
-  supervisor.use( log_citizen_output_addon );
 
 // register satellite component control code
   supervisor.register( 'brain', path.join( __dirname, '/components/brain.js' ) );
